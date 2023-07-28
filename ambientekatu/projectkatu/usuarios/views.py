d583adb5452de8d69f9f290224d6bec15ad3b610
geronilton <geronilton001@gmail.com>
2023-07-28 16:01:30 -0300

    criei forms.py, alterei views e criei register_view.html

--- ambientekatu/projectkatu/usuarios/views.py
+++ ambientekatu/projectkatu/usuarios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 # Create your views here.
+
+def register_view(request):
+    form= RegisterForm()
+    return render(request, 'pages/register_view.html', {'form' : form})
