3f4bb3152c0d6c077eca3f5c4de568bbeb08407b
geronilton <geronilton001@gmail.com>
2023-07-30 17:35:29 -0300

    criei em register_create views.py e urls.py para verificação de envio emmetodo post

--- ambientekatu/projectkatu/usuarios/urls.py
+++ ambientekatu/projectkatu/usuarios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from usuarios.views import *
 
+app_name = 'usuarios'
+
 urlpatterns = [
     path('register/', register_view, name = 'register'),
+    path('register/create', register_create, name = 'create'),
 ]
--- ambientekatu/projectkatu/usuarios/views.py
+++ ambientekatu/projectkatu/usuarios/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from .forms import RegisterForm
+from django.http import Http404
+from django.http import HttpRequest
 
 # Create your views here.
 
 def register_view(request):
+    register_form = request.session.get("register_form", None)
+
     form= RegisterForm()
+    return render(request, 'pages/register_view.html', {'form' : form})
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+    
+    post = request.POST
+    request.session["register_form"] = post
+
+    form = RegisterForm(post)
+    
+    # form= RegisterForm()
     return render(request, 'pages/register_view.html', {'form' : form})
