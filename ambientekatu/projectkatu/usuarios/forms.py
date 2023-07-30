cd1b812399e02c319169e0b34516db51530b344d
geronilton <geronilton001@gmail.com>
2023-07-30 18:19:45 -0300

    add em forms.py labels, helptexts, error mensages e widgets

--- ambientekatu/projectkatu/usuarios/forms.py
+++ ambientekatu/projectkatu/usuarios/forms.py
@@ -6,3 +6,28 @@ class RegisterForm (forms.ModelForm):
         model = User
         # fields = '__all__'
         fields = ['first_name', 'last_name', 'username', 'email','password']
+        labels = {'first_name' : 'Digite seu primeeiro nome',
+                    'last_name' : 'Digite seu ultimo nome',
+                    'username' : 'Digite seu username',
+                    'email' : 'Digite o seu email',
+                    'password' : 'Digite sua senha '
+                    }
+        help_texts = {
+            'email' : 'O email deve ter um /@',
+            'password' : 'cria uma senha forte com caracteres, numeros e simbolos especias',
+        }
+        error_mensages = {
+            'username' : {
+                'required' : 'Este campo não pode ser vazio'
+            }
+        }
+        
+        widgets = {
+            'first_name' : forms.TextInput(attrs ={ 
+                'placeholder' : 'Primeiro Nome',
+                'required' : 'true'
+        }),
+            'password' : forms.PasswordInput(attrs ={
+                'placeholder' : 'senha',
+            })
+        }
